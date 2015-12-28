4308471220840ee9e145d21e3607b33c9d2d50ae
SHIMADA Keiki <shimada.cake@gmail.com>
2015-12-29 02:40:52 +0900

    Support table attributes.

--- source.md
+++ source.md
@@ -1,26 +1,44 @@
-Example
+Examples
 ------------
-Following table was converted by `ymltbl`.
 
-````ymltbl
+Example 1. With concise classes and attributes.
+
+``` {.ymltbl caption="figure 1.1" aligns="left center right" widths="0.5 0.25 0.25"}
+-
+  - value for A
+  - value for B
+  - value for both A and B
+-
+  - False
+  - False
+  - False
+-
+  - True
+  - False
+  - False
+-
+  - False
+  - True
+  - False
+-
+  - True
+  - True
+  - True
+```
+
+Example 2. Simple notation.
+
+```ymltbl
+-
+  - FOO
+  - BAR
+  - BAZ
+-
+  - 1
+  - 2
+  - 3
 -
   - A
   - B
-  - A and B
--
-  - False
-  - False
-  - False
--
-  - True
-  - False
-  - False
--
-  - False
-  - True
-  - False
--
-  - True
-  - True
-  - True
-````
+  - C
+```
--- ymltbl.py
+++ ymltbl.py
@@ -7,35 +7,72 @@ import yaml
 def walk(key, value, format, meta):
   if key == 'CodeBlock':
     [[ident, classes, keyvals], code] = value
-    if ident == u'ymltbl':
+    if u'ymltbl' in classes:
       data = yaml.load(code)
-      return array2tbl(data)
+      attributes = list_to_dict(keyvals)
+      return array2tbl(data, attributes)
 
-def array2tbl(data):
+def array2tbl(data, attributes):
+  check_colsize(data)
   colsize = len(data[0])
   head = data[0]
   body = data[1:]
   return pandoc.Table(
-    caption(),
-    aligns(colsize),
-    widths(colsize),
+    caption(attributes),
+    aligns(colsize, attributes),
+    widths(colsize, attributes),
     headers(head),
     rows(body))
 
-def caption():
-  return []
+def caption(attributes):
+  capt = attributes.get("caption")
+  if capt
+    return [pandoc.Str(unicode(capt))]
+  else:
+    return []
 
-def aligns(size):
-  def alignDefault(): return { "t": "AlignDefault",  "c": [] }
-  return [alignDefault() for x in range(size)]
+def aligns(size, attributes):
+  """Attribute `aligns`: text-align keywords seperated with spaces.
+     If not set, all aligns are set to default (align-left).
+     :rtype: list of Pandoc json objects
+  """
+  def make_obj(label): return {"t": label, "c": []}
+  def align_default(): return make_obj("AlignDefault")
+  align_types = {
+    "right": "AlignRight",
+    "left": "AlignLeft",
+    "center": "AlignCenter",
+    "": "AlignDefault"
+  }
+  aligns = attributes.get("aligns")
+  if aligns:
+    return [make_obj(align_types[align]) for align in aligns.split(" ")]
+  else:
+    return [align_default() for x in range(size)]
 
-def widths(size):
-  return [0 for x in range(size)]
+def widths(size, attributes):
+  """Attributes `widths`: table columns width percentages seperated with spaces.
+     If not set, all widths are set to 0 (auto-sizing).
+     :rtype: list of Pandoc json objects
+  """
+  widths = attributes.get("widths")
+  if widths:
+    return [float(x) for x in widths.split(" ")]
+  else:
+    return [0 for x in range(size)]
 
 def headers(cols):
+  """
+     :param list of str cols: header columns
+     :rtype: list of Pandoc json objects
+  """
   return [parse(col) for col in cols]
 
 def rows(data):
+  """
+     :param list of list of str data: table cell strings
+     :rtype: list of Pandoc json objects
+  """
   def plainStr(s): return [pandoc.Plain([pandoc.Str(s)])]
   return [ [ parse(col) for col in cols] for cols in data]
 
@@ -48,6 +85,27 @@ def parse(string):
     result.append(pandoc.Space())
   result = result[:-1]
   return [pandoc.Plain(result)]
+
+def list_to_dict(pairlist):
+  """convert from assoc like nested arrays to dict.
+  """
+  result = {}
+  for pair in pairlist:
+    result[pair[0]] = pair[1]
+  return result
+
+def check_colsize(data):
+  col_sizes = [len(cols) for cols in data]
+  sample = col_sizes[0]
+  if all([s is sample for s in col_sizes]):
+    pass
+  else:
+    raise(TypeError("All rows MUST have same number of cols"))
+
+import sys
+
+def debug(msg):
+  sys.stderr.write("[ymltbl] DEBUG: {0}\n".format(msg))
 
 if __name__ == "__main__":
   pandoc.toJSONFilter(walk)
